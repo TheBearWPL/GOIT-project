54e8e79267f1721f6d7bc2b02b1616579002229d
drazka <izabdr@gmail.com>
2024-06-17 20:36:20 +0200

    script for mobile menu

--- src/mobile.jsx
+++
@@ -1,8 +0,0 @@
-<script>
-  const OPEN_CLASS = "is-open";
-  const mobileNav = document.querySelector("#mobile-menu");
-  const openMobileNav = () => mobileNav.classList.add(OPEN_CLASS);
-  const closeMobileNav = () => mobileNav.classList.remove(OPEN_CLASS);
-  document.querySelector("#hamburger").addEventListener("click", openMobileNav);
-  document.querySelector("#close").addEventListener("click", closeMobileNav);
-</script>
